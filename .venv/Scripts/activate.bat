f46257a6dec5dfe99fc106d8f9be776e085067d4
ig-peralta <164016134+ig-peralta@users.noreply.github.com>
2024-06-21 10:13:23 -0400

    Arreglos DJango

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\ProyectosDjango\tienda\.venv
+set VIRTUAL_ENV=c:\ProyectosDjango\tienda\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
--- core/views.py
+++ core/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import render, redirect
 from .zpoblar import poblar_bd
 from .models import Producto
 
@@ -8,8 +9,43 @@ def index(request):
 
 def ficha(request, producto_id):
     producto = Producto.objects.get(id=producto_id)
-    data = { 'producto': producto }
-    return render(request, 'core/index.html', data)
+    return render(request, 'core/ficha.html', { 'producto': producto })
+
+def registro(request):
+    return render(request, 'core/registro.html')
+
+def nosotros(request):
+    return render(request, 'core/nosotros.html')
+
+def productos(request, accion, id):
+    return render(request, 'core/productos.html')
+
+def usuarios(request, accion, id):
+    return render(request, 'core/usuarios.html')
+
+def bodega(request):
+    return render(request, 'core/bodega.html')
+
+def ventas(request):
+    return render(request, 'core/ventas.html')
+
+def boleta(request, nro_boleta):
+    return render(request, 'core/boleta.html')
+
+def ingresar(request):
+    return render(request, 'core/ingresar.html')
+
+def misdatos(request):
+    return render(request, 'core/misdatos.html')
+
+def miscompras(request):
+    return render(request, 'core/miscompras.html')
+
+def carrito(request):
+    return render(request, 'core/carrito.html')
+
+def ropa(request):
+    return render(request, 'core/ropa.html')
 
 def poblar(request):
     # Permite poblar la base de datos con valores de prueba en todas sus tablas.
